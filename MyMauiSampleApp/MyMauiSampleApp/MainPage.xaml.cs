ce85668ba11745761d3534b85ac9d7d929fe150b
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-08 15:54:03 -0800

    Add Calculator and MyMauiSimpleApp to DotnetMaui

---
+++ MyMauiSampleApp/MyMauiSampleApp/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace MyMauiSampleApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private void OnClickClear(object sender, EventArgs e)
+    {
+        count = 1;
+
+		CounterBtn.Text = $"Click Me";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
