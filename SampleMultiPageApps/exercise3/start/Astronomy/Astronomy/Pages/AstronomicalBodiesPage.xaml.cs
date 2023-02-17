d3fad70c761d3d9e72ef8f1fdc8145cab02642d9
srirambhat <43067584+srirambhat@users.noreply.github.com>
2023-02-17 13:15:06 -0800

    Add changes for MultiPages

--- SampleMultiPageApps/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
+++ SampleMultiPageApps/exercise3/start/Astronomy/Astronomy/Pages/AstronomicalBodiesPage.xaml.cs
@@ -2,9 +2,18 @@ namespace Astronomy.Pages;
 
 public partial class AstronomicalBodiesPage : ContentPage
 {
-	public AstronomicalBodiesPage()
+    public AstronomicalBodiesPage()
 	{
 		InitializeComponent();
 
+		btnComet.Clicked += async(s, e) => await Shell.Current
+                            .GoToAsync($"{nameof(AstronomicalBodyPage)}?astroName=comet");
+        btnEarth.Clicked += async (s, e) => await Shell.Current
+                            .GoToAsync($"{nameof(AstronomicalBodyPage)}?astroName=earth");
+        btnMoon.Clicked += async (s, e) => await Shell.Current
+                            .GoToAsync($"{nameof(AstronomicalBodyPage)}?astroName=moon");
+        btnSun.Clicked += async (s, e) => await Shell.Current
+                            .GoToAsync($"{nameof(AstronomicalBodyPage)}?astroName=sun");
+        
     }
 }
